b702bdaef3f60480876ce692d985322fea4b7ee8
ilmarsviksna <144133285+ilmarsviksna@users.noreply.github.com>
2024-01-18 19:08:28 +0200

    Update result.py

--- result.py
+++ result.py
@@ -1,1 +1,20 @@
-
+month = input("Kuru menesi parbaudit?")
+with open("data.csv","r") as f:
+    next(f)
+    next(f)
+    paradnieki=[[]]
+    for line in f:
+        row=line.rstrip().split(",")
+        if (row[int(month)] != "7"):
+            temp = []
+            temp.append(row[0])
+            parads = 0
+            for x in range(1, int(month)):
+                 parads = parads + int(row[x])
+            parads = 7 * int(month) - parads
+            temp.append(parads)
+            paradnieki.append(temp)
+if bool(paradnieki):
+    print("Nav paradnieku")
+else:
+    print(paradnieki)
